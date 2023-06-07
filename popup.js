18eedff61e371112159169992d884eeef859d60e
Michael Weinberg <mweinberg@nyu.edu>
2023-06-07 07:36:34 -0400

    extracts elements from form

--- popup.js
+++ popup.js
@@ -23,18 +23,91 @@ function execScript(tab) {
     chrome.scripting.executeScript(
         {
             target:{tabId: tab.id, allFrames: true},
-            func:grabImages
+            func:grabImages,
+            func:grabElements,
+            //func:grabElementsHelper(oshwaUid)
         },
         onResult
     )
 }
 
+// function grabElementsHelper(field) {
+//     let targetField = '.entity-editor__field-group[data-field-api-name="' + field + '"]'
+//     const element = document.querySelector(targetField);
+//     console.log(element);
+//     // Check if the element was found
+//     if (element) {
+//         // Access the input element within the selected div
+//         const inputElement = element.querySelector('input[data-test-id="cf-ui-text-input"]');
+        
+//         // Check if the input element was found
+//         if (inputElement) {
+//         // Extract the value
+//         const value = inputElement.value;
+//         console.log(value);
+//         } else {
+//         console.log("Input element not found.");
+//         }
+//     } else {
+//         console.log("Element not found.");
+//     }
+
+// }
+
+function grabElements() {
+    const element = document.querySelector('.entity-editor__field-group[data-field-api-name="oshwaUid"]');
+    console.log(element);
+    // Check if the element was found
+    if (element) {
+        // Access the input element within the selected div
+        const inputElement = element.querySelector('input[data-test-id="cf-ui-text-input"]');
+        
+        // Check if the input element was found
+        if (inputElement) {
+        // Extract the value
+        const value = inputElement.value;
+        console.log(value);
+        } else {
+        console.log("Input element not found.");
+        }
+    } else {
+        console.log("Element not found.");
+    }
+
+    const element1 = document.querySelector('.entity-editor__field-group[data-field-api-name="privateContact"]');
+    console.log(element1);
+    // Check if the element was found
+    if (element1) {
+        // Access the input element within the selected div
+        const inputElement = element1.querySelector('input[data-test-id="cf-ui-text-input"]');
+        
+        // Check if the input element was found
+        if (inputElement) {
+        // Extract the value
+        const value = inputElement.value;
+        console.log(value);
+        } else {
+        console.log("Input element not found.");
+        }
+    } else {
+        console.log("Element not found.");
+    }
+
+}
+
+//todo: create a function "grabelements" by pulling the first part of this out into a new function
+// then update teh execScript function to execute it too
 function grabImages() {
+    
+  
+
     //pulls a list of all img
     const images = document.querySelectorAll("img");
     //converts the list of images into an array
-    //then reuturns the urls (image.src) of the img in the array
+    //then returns the urls (image.src) of the img in the array
     return Array.from(images).map(image=>image.src);
+  
+  
 }
 
 function onResult(frames) {
